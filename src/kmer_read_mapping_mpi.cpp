cf1f6357c6d1e9c03b2c34f5b6142c9167b3093a
Lizhen Shi <lizhen9.shi@gmail.com>
2020-07-28 00:25:05 -0400

    fix

--- src/kmer_read_mapping_mpi.cpp
+++ src/kmer_read_mapping_mpi.cpp
@@ -106,7 +106,12 @@ int run(const std::vector<std::string> &input, Config &config);
 int main(int argc, char **argv) {
 	int rank, size;
 
-	MPI_Init(&argc, &argv);
+	int provided;
+	MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+	if (provided == MPI_THREAD_SINGLE) {
+		fprintf(stderr, "Could not initialize with thread support\n");
+		MPI_Abort(MPI_COMM_WORLD, 1);
+	}
 
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
--- src/merge_intrmed_mpi.cpp
+++ src/merge_intrmed_mpi.cpp
@@ -121,7 +121,12 @@ void reshuffle_rank(Config &config);
 int main(int argc, char **argv) {
 	int rank, size;
 
-	MPI_Init(&argc, &argv);
+	int provided;
+	MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+	if (provided == MPI_THREAD_SINGLE) {
+		fprintf(stderr, "Could not initialize with thread support\n");
+		MPI_Abort(MPI_COMM_WORLD, 1);
+	}
 
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
